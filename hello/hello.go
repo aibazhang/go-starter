adda01c323fa12f379237fb668c5c502dea9b14d
aibazhang <dtjp@tutanota.com>
2021-11-08 17:14:24 +0900

    Add package greeting

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	// Set properties of the predefine Logger, include
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request a greeting message
+	message, err := greetings.Hellos(names)
+
+	// If an error was returned, print it to the console and
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was return, print the returned message
+	// to the console
+	fmt.Print(message)
 }
